1d17a4535d440eab8651f5ebc51f2d775480aa43
Rolf Haenni <rolf.haenni@bfh.ch>
2013-11-19 14:32:36 +0100

    AbstractElement, Element interface

--- src/main/java/ch/bfh/unicrypt/crypto/schemes/scheme/interfaces/Scheme.java
+++ src/main/java/ch/bfh/unicrypt/crypto/schemes/scheme/interfaces/Scheme.java
@@ -2,16 +2,8 @@ package ch.bfh.unicrypt.crypto.schemes.scheme.interfaces;
 
 import ch.bfh.unicrypt.math.algebra.general.interfaces.Set;
 
-/**
- *
- * @author rolfhaenni
- */
 public interface Scheme {
 
-  /**
-   *
-   * @return
-   */
   public Set getMessageSpace();
 
 }
