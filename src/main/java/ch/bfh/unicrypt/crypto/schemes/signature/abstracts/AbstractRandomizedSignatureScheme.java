991086e2d77afc53ddd51bdbd5632fc8dcc13109
reto <knr1.bfh.ch@gmail.com>
2013-11-21 13:31:24 +0100

    Klassen auskommentiert, damit eine Kompilation möglich wird

--- src/main/java/ch/bfh/unicrypt/crypto/schemes/signature/abstracts/AbstractRandomizedSignatureScheme.java
+++ src/main/java/ch/bfh/unicrypt/crypto/schemes/signature/abstracts/AbstractRandomizedSignatureScheme.java
@@ -1,24 +1,25 @@
 package ch.bfh.unicrypt.crypto.schemes.signature.abstracts;
 
-import java.util.Random;
-
-import ch.bfh.unicrypt.crypto.schemes.signature.interfaces.RandomizedSignatureScheme;
-
-public abstract class AbstractRandomizedSignatureScheme extends AbstractSignatureScheme implements RandomizedSignatureScheme {
-
-  @Override
-  public Element sign(final Element privateKey, final Element message) {
-    return this.sign(privateKey, message, (Random) null);
-  }
-
-  @Override
-  public Element sign(final Element privateKey, final Element message, final Random random) {
-    return this.sign(privateKey, message, this.getRandomizationSpace().getRandomElement(random));
-  }
-
-  @Override
-  public Element sign(final Element privateKey, final Element message, final Element randomization) {
-    return this.getSignatureFunction().apply(privateKey, message, randomization);
-  }
-
+public abstract class AbstractRandomizedSignatureScheme {
+//import java.util.Random;
+//
+//import ch.bfh.unicrypt.crypto.schemes.signature.interfaces.RandomizedSignatureScheme;
+//
+//public abstract class AbstractRandomizedSignatureScheme extends AbstractSignatureScheme implements RandomizedSignatureScheme {
+//
+//  @Override
+//  public Element sign(final Element privateKey, final Element message) {
+//    return this.sign(privateKey, message, (Random) null);
+//  }
+//
+//  @Override
+//  public Element sign(final Element privateKey, final Element message, final Random random) {
+//    return this.sign(privateKey, message, this.getRandomizationSpace().getRandomElement(random));
+//  }
+//
+//  @Override
+//  public Element sign(final Element privateKey, final Element message, final Element randomization) {
+//    return this.getSignatureFunction().apply(privateKey, message, randomization);
+//  }
+//
 }
--- src/main/java/ch/bfh/unicrypt/crypto/schemes/signature/abstracts/AbstractSignatureScheme.java
+++ src/main/java/ch/bfh/unicrypt/crypto/schemes/signature/abstracts/AbstractSignatureScheme.java
@@ -1,31 +1,32 @@
 package ch.bfh.unicrypt.crypto.schemes.signature.abstracts;
 
-import ch.bfh.unicrypt.crypto.schemes.signature.interfaces.SignatureScheme;
-import ch.bfh.unicrypt.math.algebra.general.classes.BooleanElement;
-import ch.bfh.unicrypt.math.algebra.general.classes.BooleanSet;
-import ch.bfh.unicrypt.math.algebra.general.interfaces.Element;
-import ch.bfh.unicrypt.math.algebra.general.interfaces.Set;
-
-public abstract class AbstractSignatureScheme<M extends Set, S extends Set, E extends Element> implements SignatureScheme {
-
-  @Override
-  public E sign(final Element privateKey, final Element message) {
-    return (E) this.getSignatureFunction().apply(privateKey, message);
-  }
-
-  @Override
-  public BooleanElement verify(final Element publicKey, final Element message, final Element signature) {
-    return this.getVerificationFunction().apply(publicKey, message, signature).isEqual(BooleanSet.TRUE);
-  }
-
-  @Override
-  public M getMessageSpace() {
-    return ZPlus.Factory.getInstance();
-  }
-
-  @Override
-  public S getSignatureSpace() {
-    return (S) this.getSignatureFunction().getCoDomain();
-  }
+public abstract class AbstractSignatureScheme {
+//import ch.bfh.unicrypt.crypto.schemes.signature.interfaces.SignatureScheme;
+//import ch.bfh.unicrypt.math.algebra.general.classes.BooleanElement;
+//import ch.bfh.unicrypt.math.algebra.general.classes.BooleanSet;
+//import ch.bfh.unicrypt.math.algebra.general.interfaces.Element;
+//import ch.bfh.unicrypt.math.algebra.general.interfaces.Set;
+//
+//public abstract class AbstractSignatureScheme<M extends Set, S extends Set, E extends Element> implements SignatureScheme {
+//
+//  @Override
+//  public E sign(final Element privateKey, final Element message) {
+//    return (E) this.getSignatureFunction().apply(privateKey, message);
+//  }
+//
+//  @Override
+//  public BooleanElement verify(final Element publicKey, final Element message, final Element signature) {
+//    return this.getVerificationFunction().apply(publicKey, message, signature).isEqual(BooleanSet.TRUE);
+//  }
+//
+//  @Override
+//  public M getMessageSpace() {
+//    return ZPlus.Factory.getInstance();
+//  }
+//
+//  @Override
+//  public S getSignatureSpace() {
+//    return (S) this.getSignatureFunction().getCoDomain();
+//  }
 
 }
