a52cad6bb397f1dd43d91360dfd35c4b56f6bebf
reto <knr1.bfh.ch@gmail.com>
2013-11-24 21:08:32 +0100

    Klassen auskommentiert, damit eine Kompilation möglich wird

--- src/main/java/ch/bfh/unicrypt/crypto/schemes/blinding/abstracts/AbstractBlindingScheme.java
+++ src/main/java/ch/bfh/unicrypt/crypto/schemes/blinding/abstracts/AbstractBlindingScheme.java
@@ -1,74 +1,75 @@
 package ch.bfh.unicrypt.crypto.schemes.blinding.abstracts;
 
-import java.util.Random;
-
-import ch.bfh.unicrypt.crypto.schemes.blinding.interfaces.BlindingScheme;
-import ch.bfh.unicrypt.math.algebra.general.interfaces.Group;
-import ch.bfh.unicrypt.math.function.interfaces.Function;
-
-public abstract class AbstractBlindingScheme implements BlindingScheme {
-
-  Group blindingSpace;
-  Group blindingValueSpace;
-  Function blindingFunction;
-  Function unblindingFunction;
-
-  protected AbstractBlindingScheme(final Group blindingSpace, Group blindingValueSpace) {
-    if (blindingSpace == null || blindingValueSpace == null) {
-      throw new IllegalArgumentException();
-    }
-    this.blindingSpace = blindingSpace;
-    this.blindingValueSpace = blindingValueSpace;
-  }
-
-  @Override
-  public Element blind(final Element value) {
-    return this.blind(value, (Random) null);
-  }
-
-  @Override
-  public Element blind(final Element value, final Random random) {
-    return this.blind(value, this.getBlindingValueSpace().getRandomElement(random));
-  }
-
-  @Override
-  public Element blind(final Element value, final Element blindingValue) {
-    return this.getBlindingFunction().apply(value, blindingValue);
-  }
-
-  @Override
-  public Element unblind(final Element blindedValue, final Element blindingValue) {
-    return this.getUnblindingFunction().apply(blindedValue, blindingValue);
-  }
-
-  @Override
-  public Group getBlindingSpace() {
-    return this.blindingSpace;
-  }
-
-  @Override
-  public Group getBlindingValueSpace() {
-    return this.blindingValueSpace;
-  }
-
-  @Override
-  public Function getBlindingFunction() {
-    return this.blindingFunction;
-  }
-
-  @Override
-  public Function getBlindingFunction(final Element value) {
-    return this.getBlindingFunction().partiallyApply(value, 0);
-  }
-
-  @Override
-  public Function getUnblindingFunction() {
-    return this.unblindingFunction;
-  }
-
-  @Override
-  public Function getUnblindingFunction(final Element value) {
-    return this.getUnblindingFunction().partiallyApply(value, 0);
-  }
+public abstract class AbstractBlindingScheme {
+//import java.util.Random;
+//
+//import ch.bfh.unicrypt.crypto.schemes.blinding.interfaces.BlindingScheme;
+//import ch.bfh.unicrypt.math.algebra.general.interfaces.Group;
+//import ch.bfh.unicrypt.math.function.interfaces.Function;
+//
+//public abstract class AbstractBlindingScheme implements BlindingScheme {
+//
+//  Group blindingSpace;
+//  Group blindingValueSpace;
+//  Function blindingFunction;
+//  Function unblindingFunction;
+//
+//  protected AbstractBlindingScheme(final Group blindingSpace, Group blindingValueSpace) {
+//    if (blindingSpace == null || blindingValueSpace == null) {
+//      throw new IllegalArgumentException();
+//    }
+//    this.blindingSpace = blindingSpace;
+//    this.blindingValueSpace = blindingValueSpace;
+//  }
+//
+//  @Override
+//  public Element blind(final Element value) {
+//    return this.blind(value, (Random) null);
+//  }
+//
+//  @Override
+//  public Element blind(final Element value, final Random random) {
+//    return this.blind(value, this.getBlindingValueSpace().getRandomElement(random));
+//  }
+//
+//  @Override
+//  public Element blind(final Element value, final Element blindingValue) {
+//    return this.getBlindingFunction().apply(value, blindingValue);
+//  }
+//
+//  @Override
+//  public Element unblind(final Element blindedValue, final Element blindingValue) {
+//    return this.getUnblindingFunction().apply(blindedValue, blindingValue);
+//  }
+//
+//  @Override
+//  public Group getBlindingSpace() {
+//    return this.blindingSpace;
+//  }
+//
+//  @Override
+//  public Group getBlindingValueSpace() {
+//    return this.blindingValueSpace;
+//  }
+//
+//  @Override
+//  public Function getBlindingFunction() {
+//    return this.blindingFunction;
+//  }
+//
+//  @Override
+//  public Function getBlindingFunction(final Element value) {
+//    return this.getBlindingFunction().partiallyApply(value, 0);
+//  }
+//
+//  @Override
+//  public Function getUnblindingFunction() {
+//    return this.unblindingFunction;
+//  }
+//
+//  @Override
+//  public Function getUnblindingFunction(final Element value) {
+//    return this.getUnblindingFunction().partiallyApply(value, 0);
+//  }
 
 }
--- src/main/java/ch/bfh/unicrypt/crypto/schemes/blinding/classes/StandardBlindingScheme.java
+++ src/main/java/ch/bfh/unicrypt/crypto/schemes/blinding/classes/StandardBlindingScheme.java
@@ -1,36 +1,35 @@
 package ch.bfh.unicrypt.crypto.schemes.blinding.classes;
 
-import ch.bfh.unicrypt.crypto.schemes.blinding.abstracts.AbstractBlindingScheme;
-import ch.bfh.unicrypt.math.algebra.additive.classes.ZPlusMod;
-import ch.bfh.unicrypt.math.algebra.general.interfaces.Group;
-import ch.bfh.unicrypt.math.function.classes.SelfApplyFunction;
-
-public class StandardBlindingScheme extends AbstractBlindingScheme {
-
-  Group blindingSpace;
-  ZPlusMod blindingValueSpace;
-  SelfApplyFunction blindingFunction;
-
-  public StandardBlindingScheme(final Group blindingSpace) {
-    if (blindingSpace == null)
-      throw new IllegalArgumentException();
-    this.blindingSpace = blindingSpace;
-    this.blindingValueSpace = blindingSpace.getZModOrder();
-    this.blindingFunction = new SelfApplyFunction(this.blindingSpace, this.blindingValueSpace);
-  }
-
-
-  // @Override
-  // public Element unblind(final Element value, final AdditiveElement
-  // blindingValue) {
-  // if(blindingValue==null)
-  // throw new IllegalArgumentException();
-  // return this.blind(value, blindingValue.invert());
-  // That does not work, as the blindingValue is an element of an additive
-  // group. Hence blinding again with the
-  // inverse value always results in 1
-  // (x^y)^{-y}=x^{y-y}=x^0=1
-  // }
-
-
+public class StandardBlindingScheme {
+//import ch.bfh.unicrypt.crypto.schemes.blinding.abstracts.AbstractBlindingScheme;
+//import ch.bfh.unicrypt.math.algebra.additive.classes.ZPlusMod;
+//import ch.bfh.unicrypt.math.algebra.general.interfaces.Group;
+//import ch.bfh.unicrypt.math.function.classes.SelfApplyFunction;
+//
+//public class StandardBlindingScheme extends AbstractBlindingScheme {
+//
+//    Group blindingSpace;
+//    ZPlusMod blindingValueSpace;
+//    SelfApplyFunction blindingFunction;
+//
+//    public StandardBlindingScheme(final Group blindingSpace) {
+//	if (blindingSpace == null) {
+//	    throw new IllegalArgumentException();
+//	}
+//	this.blindingSpace = blindingSpace;
+//	this.blindingValueSpace = blindingSpace.getZModOrder();
+//	this.blindingFunction = new SelfApplyFunction(this.blindingSpace, this.blindingValueSpace);
+//    }
+//
+//  // @Override
+//    // public Element unblind(final Element value, final AdditiveElement
+//    // blindingValue) {
+//    // if(blindingValue==null)
+//    // throw new IllegalArgumentException();
+//    // return this.blind(value, blindingValue.invert());
+//    // That does not work, as the blindingValue is an element of an additive
+//    // group. Hence blinding again with the
+//    // inverse value always results in 1
+//    // (x^y)^{-y}=x^{y-y}=x^0=1
+//    // }
 }
