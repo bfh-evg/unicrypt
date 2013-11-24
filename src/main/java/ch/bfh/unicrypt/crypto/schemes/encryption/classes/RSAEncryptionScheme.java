f42de3f22ec0013a5ef63a4260f9adb2673b54cb
reto <knr1.bfh.ch@gmail.com>
2013-11-24 21:10:46 +0100

    Klassen auskommentiert, damit eine Kompilation möglich wird

--- src/main/java/ch/bfh/unicrypt/crypto/schemes/encryption/classes/RSAEncryptionScheme.java
+++ src/main/java/ch/bfh/unicrypt/crypto/schemes/encryption/classes/RSAEncryptionScheme.java
@@ -4,36 +4,37 @@
  */
 package ch.bfh.unicrypt.crypto.schemes.encryption.classes;
 
-import ch.bfh.unicrypt.crypto.schemes.encryption.abstracts.AbstractAsymmetricEncryptionScheme;
-import ch.bfh.unicrypt.crypto.keygenerator.interfaces.KeyPairGenerator;
-import ch.bfh.unicrypt.math.algebra.multiplicative.classes.ZStarMod;
-import ch.bfh.unicrypt.math.algebra.multiplicative.classes.ZTimesMod;
-import ch.bfh.unicrypt.math.algebra.multiplicative.classes.ZTimesModElement;
-import ch.bfh.unicrypt.math.algebra.dualistic.classes.ZModPrimes;
-import ch.bfh.unicrypt.math.function.interfaces.Function;
-
-/**
- *
- * @author rolfhaenni
- */
-public class RSAEncryptionScheme extends AbstractAsymmetricEncryptionScheme<ZTimesMod, ZTimesMod, ZTimesModElement, ZTimesModElement> {
-
-  protected RSAEncryptionScheme(KeyPairGenerator keyPairGenerator, Function encryptionFunction, Function decryptionFunction) {
-    super(keyPairGenerator, encryptionFunction, decryptionFunction);
-  }
-
-  public static RSAEncryptionScheme getInstance(ZModPrimes zTimesMod) {
-    if (zTimesMod == null) {
-      throw new IllegalArgumentException();
-    }
-    Function encryptionFunction
-    return new RSAEncryptionScheme
-
-  }
-
-  @Override
-  protected String standardToStringContent() {
-    return this.getPlaintextSpace().toString();
-  }
+public class RSAEncryptionScheme {
+//import ch.bfh.unicrypt.crypto.schemes.encryption.abstracts.AbstractAsymmetricEncryptionScheme;
+//import ch.bfh.unicrypt.crypto.keygenerator.interfaces.KeyPairGenerator;
+//import ch.bfh.unicrypt.math.algebra.multiplicative.classes.ZStarMod;
+//import ch.bfh.unicrypt.math.algebra.multiplicative.classes.ZTimesMod;
+//import ch.bfh.unicrypt.math.algebra.multiplicative.classes.ZTimesModElement;
+//import ch.bfh.unicrypt.math.algebra.dualistic.classes.ZModPrimes;
+//import ch.bfh.unicrypt.math.function.interfaces.Function;
+//
+///**
+// *
+// * @author rolfhaenni
+// */
+//public class RSAEncryptionScheme extends AbstractAsymmetricEncryptionScheme<ZTimesMod, ZTimesMod, ZTimesModElement, ZTimesModElement> {
+//
+//  protected RSAEncryptionScheme(KeyPairGenerator keyPairGenerator, Function encryptionFunction, Function decryptionFunction) {
+//    super(keyPairGenerator, encryptionFunction, decryptionFunction);
+//  }
+//
+//  public static RSAEncryptionScheme getInstance(ZModPrimes zTimesMod) {
+//    if (zTimesMod == null) {
+//      throw new IllegalArgumentException();
+//    }
+//    Function encryptionFunction
+//    return new RSAEncryptionScheme
+//
+//  }
+//
+//  @Override
+//  protected String standardToStringContent() {
+//    return this.getPlaintextSpace().toString();
+//  }
 
 }
