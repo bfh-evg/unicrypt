bac10ccb8ec3e4958fd552ec0dde02fce894cf54
philipp <philipp@radisli.bfh.ch>
2013-11-26 09:29:49 +0100

    Changed standardIsEqual to abstractIsEqual

--- src/main/java/ch/bfh/unicrypt/crypto/encoder/classes/GStarModSafePrimeEncoder.java
+++ src/main/java/ch/bfh/unicrypt/crypto/encoder/classes/GStarModSafePrimeEncoder.java
@@ -13,7 +13,7 @@ import java.math.BigInteger;
 import java.util.Random;
 
 public class GStarModSafePrimeEncoder
-			 extends AbstractEncoder<ZModPrime, GStarModSafePrime, ZModElement, GStarModElement> {
+	   extends AbstractEncoder<ZModPrime, GStarModSafePrime, ZModElement, GStarModElement> {
 
 	private final GStarModSafePrime gStarModSafePrime;
 
@@ -43,7 +43,7 @@ public class GStarModSafePrimeEncoder
 	}
 
 	static class EncodingFunction
-				 extends AbstractFunction<ZModPrime, GStarModSafePrime, GStarModElement> {
+		   extends AbstractFunction<ZModPrime, GStarModSafePrime, GStarModElement> {
 
 		protected EncodingFunction(final ZModPrime domain, final GStarMod coDomain) {
 			super(domain, coDomain);
@@ -59,10 +59,15 @@ public class GStarModSafePrimeEncoder
 			return coDomain.getElement(coDomain.getModulus().subtract(value));
 		}
 
+		@Override
+		protected boolean abstractIsEqual(Function function) {
+			return true;
+		}
+
 	}
 
 	static class DecodingFunction
-				 extends AbstractFunction<GStarModSafePrime, ZModPrime, ZModElement> {
+		   extends AbstractFunction<GStarModSafePrime, ZModPrime, ZModElement> {
 
 		protected DecodingFunction(final GStarMod domain, final ZModPrime coDomain) {
 			super(domain, coDomain);
@@ -76,6 +81,11 @@ public class GStarModSafePrimeEncoder
 				return this.getCoDomain().getElement(value.subtract(BigInteger.ONE));
 			}
 			return this.getCoDomain().getElement((domain.getModulus().subtract(value)).subtract(BigInteger.ONE));
+		}
+
+		@Override
+		protected boolean abstractIsEqual(Function function) {
+			return true;
 		}
 
 	}
