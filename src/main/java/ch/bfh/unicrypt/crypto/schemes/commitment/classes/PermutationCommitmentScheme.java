0cea130fd082ceff448749f27d193afacd74bae8
philipp <philipp.locher@bfh.ch>
2013-12-10 18:13:01 +0100

    Extended public Interface of PermutationCommitmentScheme

--- src/main/java/ch/bfh/unicrypt/crypto/schemes/commitment/classes/PermutationCommitmentScheme.java
+++ src/main/java/ch/bfh/unicrypt/crypto/schemes/commitment/classes/PermutationCommitmentScheme.java
@@ -18,69 +18,85 @@ import java.util.Random;
 
 // RS: ProductZModPrime does not exist, so ProductGroup
 public class PermutationCommitmentScheme
-	extends AbstractRandomizedCommitmentScheme<PermutationGroup, PermutationElement, ProductGroup, Tuple, ProductGroup> {
+	   extends AbstractRandomizedCommitmentScheme<PermutationGroup, PermutationElement, ProductGroup, Tuple, ProductGroup> {
 
-    private final CyclicGroup cyclicGroup;
-    private final Element randomizationGenerator;
-    private final Element[] messageGenerators;
-
-    protected PermutationCommitmentScheme(CyclicGroup cyclicGroup, Element randomizationGenerator, Element[] messageGenerators) {
-	this.cyclicGroup = cyclicGroup;
-	this.randomizationGenerator = randomizationGenerator;
-	this.messageGenerators = messageGenerators;
-    }
-
-    @Override
-    protected Function abstractGetCommitmentFunction() {
-	return new PermutationCommitmentFunction(this.cyclicGroup, this.randomizationGenerator, this.messageGenerators);
-    }
-
-    public static PermutationCommitmentScheme getInstance(final CyclicGroup cyclicGroup, final int size) {
-	return PermutationCommitmentScheme.getInstance(cyclicGroup, size, (RandomOracle) null);
-    }
-
-    public static PermutationCommitmentScheme getInstance(final CyclicGroup cyclicGroup, final int size, RandomOracle randomOracle) {
-	if (randomOracle == null) {
-	    randomOracle = RandomOracle.DEFAULT;
-	}
-	final Element randomizationGenerator = cyclicGroup.getIndependentGenerator(0, randomOracle);
-	final Element[] messageGenerators = (Element[]) Array.newInstance(Element.class, size);
-	for (int i = 0; i < size; i++) {
-	    messageGenerators[i] = (Element) cyclicGroup.getIndependentGenerator(i + 1, randomOracle);
-	}
-
-	return new PermutationCommitmentScheme(cyclicGroup, randomizationGenerator, messageGenerators);
-    }
-
-    private class PermutationCommitmentFunction
-	    extends AbstractFunction<ProductSet, Pair, ProductGroup, Tuple> {
-
+	private final CyclicGroup cyclicGroup;
 	private final Element randomizationGenerator;
 	private final Element[] messageGenerators;
 
-	protected PermutationCommitmentFunction(CyclicGroup cyclicGroup, Element randomizationGenerator, Element[] messageGenerators) {
-	    super(ProductSet.getInstance(PermutationGroup.getInstance(messageGenerators.length),
-					 ProductGroup.getInstance(cyclicGroup.getZModOrder(), messageGenerators.length)),
-		  ProductGroup.getInstance(cyclicGroup, messageGenerators.length));
-
-	    this.randomizationGenerator = randomizationGenerator;
-	    this.messageGenerators = messageGenerators;
+	protected PermutationCommitmentScheme(CyclicGroup cyclicGroup, Element randomizationGenerator, Element[] messageGenerators) {
+		this.cyclicGroup = cyclicGroup;
+		this.randomizationGenerator = randomizationGenerator;
+		this.messageGenerators = messageGenerators;
 	}
 
 	@Override
-	protected Tuple abstractApply(Pair element, Random random) {
-	    final Permutation permutation = ((PermutationElement) element.getFirst()).getPermutation();
-	    final Tuple randomizations = (Tuple) element.getSecond();
-	    Element[] ret = new Element[this.messageGenerators.length];
-	    for (int i = 0; i < this.messageGenerators.length; i++) {
-		ret[i] = this.randomizationGenerator
-			.selfApply(randomizations.getAt(i))
-			.apply(this.messageGenerators[permutation.permute(i)]);
-	    }
-
-	    return Tuple.getInstance(ret);
+	protected Function abstractGetCommitmentFunction() {
+		return new PermutationCommitmentFunction(this.cyclicGroup, this.randomizationGenerator, this.messageGenerators);
 	}
 
-    }
+	public CyclicGroup getCyclicGroup() {
+		return this.cyclicGroup;
+	}
+
+	public int getSize() {
+		return this.messageGenerators.length;
+	}
+
+	public Element getRandomizationGenerator() {
+		return this.randomizationGenerator;
+	}
+
+	public Element[] getMessageGenerators() {
+		return this.messageGenerators.clone();
+	}
+
+	public static PermutationCommitmentScheme getInstance(final CyclicGroup cyclicGroup, final int size) {
+		return PermutationCommitmentScheme.getInstance(cyclicGroup, size, (RandomOracle) null);
+	}
+
+	public static PermutationCommitmentScheme getInstance(final CyclicGroup cyclicGroup, final int size, RandomOracle randomOracle) {
+		if (randomOracle == null) {
+			randomOracle = RandomOracle.DEFAULT;
+		}
+		final Element randomizationGenerator = cyclicGroup.getIndependentGenerator(0, randomOracle);
+		final Element[] messageGenerators = (Element[]) Array.newInstance(Element.class, size);
+		for (int i = 0; i < size; i++) {
+			messageGenerators[i] = (Element) cyclicGroup.getIndependentGenerator(i + 1, randomOracle);
+		}
+
+		return new PermutationCommitmentScheme(cyclicGroup, randomizationGenerator, messageGenerators);
+	}
+
+	private class PermutationCommitmentFunction
+		   extends AbstractFunction<ProductSet, Pair, ProductGroup, Tuple> {
+
+		private final Element randomizationGenerator;
+		private final Element[] messageGenerators;
+
+		protected PermutationCommitmentFunction(CyclicGroup cyclicGroup, Element randomizationGenerator, Element[] messageGenerators) {
+			super(ProductSet.getInstance(PermutationGroup.getInstance(messageGenerators.length),
+										 ProductGroup.getInstance(cyclicGroup.getZModOrder(), messageGenerators.length)),
+				  ProductGroup.getInstance(cyclicGroup, messageGenerators.length));
+
+			this.randomizationGenerator = randomizationGenerator;
+			this.messageGenerators = messageGenerators;
+		}
+
+		@Override
+		protected Tuple abstractApply(Pair element, Random random) {
+			final Permutation permutation = ((PermutationElement) element.getFirst()).getPermutation();
+			final Tuple randomizations = (Tuple) element.getSecond();
+			Element[] ret = new Element[this.messageGenerators.length];
+			for (int i = 0; i < this.messageGenerators.length; i++) {
+				ret[i] = this.randomizationGenerator
+					   .selfApply(randomizations.getAt(i))
+					   .apply(this.messageGenerators[permutation.permute(i)]);
+			}
+
+			return Tuple.getInstance(ret);
+		}
+
+	}
 
 }
