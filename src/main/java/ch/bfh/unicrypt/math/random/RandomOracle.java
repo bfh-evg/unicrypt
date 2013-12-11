ff10b1c78300eb9509910d2cad0d7d5cb5b67bf5
philipp <philipp.locher@bfh.ch>
2013-12-11 11:12:33 +0100

    Added RandomOracle.getRandom(byte[] query)

--- src/main/java/ch/bfh/unicrypt/math/random/RandomOracle.java
+++ src/main/java/ch/bfh/unicrypt/math/random/RandomOracle.java
@@ -7,9 +7,6 @@ package ch.bfh.unicrypt.math.random;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 
 /**
  *
@@ -17,38 +14,50 @@ import java.util.Random;
  */
 public class RandomOracle {
 
-  private static final String DEFAULT_ALGORITHM_NAME = "SHA1PRNG";
-  public static final RandomOracle DEFAULT = RandomOracle.getInstance(DEFAULT_ALGORITHM_NAME);
+	private static final String DEFAULT_ALGORITHM_NAME = "SHA1PRNG";
+	public static final RandomOracle DEFAULT = RandomOracle.getInstance(DEFAULT_ALGORITHM_NAME);
+	private final String algorithmName;
 
-  private final String algorithmName;
+	protected RandomOracle(String algorithmName) {
+		this.algorithmName = algorithmName;
+	}
 
-  protected RandomOracle(String algorithmName) {
-    this.algorithmName = algorithmName;
-  }
+	public SecureRandom getRandom(long query) {
+		if (query < 0) {
+			throw new IllegalArgumentException();
+		}
+		try {
+			SecureRandom random = SecureRandom.getInstance(this.getAlgorithmName());
+			random.setSeed(query + 1); // to avoid 0 being ignored as seed
+			return random;
+		} catch (final NoSuchAlgorithmException e) {
+			throw new InternalError();
+		}
+	}
 
-  public SecureRandom getRandom(long query) {
-    if (query < 0) {
-      throw new IllegalArgumentException();
-    }
-    try {
-      SecureRandom random = SecureRandom.getInstance(this.getAlgorithmName());
-      random.setSeed(query + 1); // to avoid 0 being ignored as seed
-      return random;
-    } catch (final NoSuchAlgorithmException e) {
-      throw new InternalError();
-    }
-  }
+	public SecureRandom getRandom(byte[] query) {
+		if (query == null || query.length < 1) {
+			throw new IllegalArgumentException();
+		}
+		try {
+			SecureRandom random = SecureRandom.getInstance(this.getAlgorithmName());
+			random.setSeed(query);
+			return random;
+		} catch (final NoSuchAlgorithmException e) {
+			throw new InternalError();
+		}
+	}
 
-  public String getAlgorithmName() {
-    return this.algorithmName;
-  }
+	public String getAlgorithmName() {
+		return this.algorithmName;
+	}
 
-  public static RandomOracle getInstance() {
-    return RandomOracle.DEFAULT;
-  }
+	public static RandomOracle getInstance() {
+		return RandomOracle.DEFAULT;
+	}
 
-  public static RandomOracle getInstance(String algorithmName) {
-    return new RandomOracle(algorithmName);
-  }
+	public static RandomOracle getInstance(String algorithmName) {
+		return new RandomOracle(algorithmName);
+	}
 
 }
