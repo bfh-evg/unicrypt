b7d82bcad58caef95dac501627265f809aa0e3d2
reto <knr1.bfh.ch@gmail.com>
2014-04-23 11:11:04 +0200

    Example refined in order to meet the critics provided by the users of the library:
    A first example that shows the use in a 'slim' shot (-> simplicity)
    Further examples show the usage of it by dividing the different steps in different contexts. (-> usage)

--- examples/src/main/java/ch/bfh/unicrypt/crypto/schemes/commitment/PedersenCommitmentExample.java
+++ examples/src/main/java/ch/bfh/unicrypt/crypto/schemes/commitment/PedersenCommitmentExample.java
@@ -43,10 +43,13 @@ package ch.bfh.unicrypt.crypto.schemes.commitment;
 
 import ch.bfh.unicrypt.Example;
 import ch.bfh.unicrypt.crypto.schemes.commitment.classes.PedersenCommitmentScheme;
+import ch.bfh.unicrypt.helper.array.ByteArray;
 import ch.bfh.unicrypt.math.algebra.general.classes.BooleanElement;
 import ch.bfh.unicrypt.math.algebra.general.interfaces.CyclicGroup;
 import ch.bfh.unicrypt.math.algebra.general.interfaces.Element;
 import ch.bfh.unicrypt.math.algebra.multiplicative.classes.GStarModSafePrime;
+import ch.bfh.unicrypt.random.classes.ReferenceRandomByteSequence;
+import java.math.BigInteger;
 
 /**
  *
@@ -56,7 +59,7 @@ public class PedersenCommitmentExample {
 
 	public static void example1() {
 
-		// Create cyclic group G_q (modulo 167) and wth generator=98
+		// Create cyclic group G_q (modulo 167)
 		CyclicGroup cyclicGroup = GStarModSafePrime.getInstance(167);
 
 		// Create commitment scheme to be used
@@ -73,10 +76,222 @@ public class PedersenCommitmentExample {
 		BooleanElement result = commitmentScheme.decommit(message, randomization, commitment);
 
 		Example.printLine("Cylic Group", cyclicGroup);
+		Example.printLine("Message Generator", commitmentScheme.getMessageGenerator());
+		Example.printLine("Randomization Generator", commitmentScheme.getRandomizationGenerator());
 		Example.printLine("Message", message);
 		Example.printLine("Randomization", randomization);
 		Example.printLine("Commitment", commitment);
 		Example.printLine("Result", result);
+	}
+
+	public static void example2() {
+		//This example demonstrates the different steps in three Contexts:
+		// Here it is assumed that the generators and the randomization are provided from 'outside'
+
+		//1. Public parameters. Within this context the math and the generators are defined.
+		//2. Commitment. This sub-context covers the commitment phase.
+		//3. Decommitment. This sub-context cover the decommitment phase.
+		//Public context
+		{
+			// Create cyclic group G_q (modulo 167)
+			CyclicGroup cyclicGroup = GStarModSafePrime.getInstance(167);
+
+			// Create two independent generators g=98, h=27;
+			Element messageGenerator = cyclicGroup.getElementFrom(BigInteger.valueOf(27));
+			Element randomElementGenerator = cyclicGroup.getElementFrom(BigInteger.valueOf(98)); //h=g^2 \mod{167} not really independent
+
+			Example.printLine("Cylic Group (G_q)", cyclicGroup);
+			Example.printLine("Message Generator (g)", messageGenerator);
+			Example.printLine("Randomization Generator (h)", randomElementGenerator);
+
+			Element commitmentElement = null;
+			Element randomizationElement = null;
+			Element messageElement = null;
+
+			// Commitment context
+			{
+				// Create commitment scheme to be used
+				PedersenCommitmentScheme commitmentScheme = PedersenCommitmentScheme.getInstance(randomElementGenerator, messageGenerator);
+
+				// Create message (m) and randomization (r) to commit
+				Element message = commitmentScheme.getMessageSpace().getElementFrom(42);
+				Element randomization = commitmentScheme.getRandomizationSpace().getElementFrom(7); //Not really random!
+
+				// Create commitment (h^m * g^r mod p)
+				Element commitment = commitmentScheme.commit(message, randomization);
+
+				//Publish commitment
+				commitmentElement = commitment;
+
+				Example.printLine("Commitment", commitment);
+
+				//Publish for decommitment
+				randomizationElement = randomization;
+				messageElement = message;
+			}
+
+			//Decommitment context
+			{
+
+				// (Re-)Create commitment scheme to be used
+				PedersenCommitmentScheme commitmentScheme = PedersenCommitmentScheme.getInstance(randomElementGenerator, messageGenerator);
+
+				// Get the commitment
+				Element commitment = commitmentElement;
+
+				// Get the message (m)
+				Element message = messageElement;
+
+				// Get the randomization (r)
+				Element randomization = randomizationElement;
+
+				// Decommit
+				BooleanElement result = commitmentScheme.decommit(message, randomization, commitment);
+
+				Example.printLine("Result", result);
+			}
+		}
+	}
+
+	public static void example3() {
+		// Here the generators are chosen by the use of a common reference string (consensed from outside).
+		// randomization is provided from 'outside'
+
+		//This example demonstrates the different steps in three Contexts:
+		//1. Public parameters. Within this context the math and the common reference string is defined.
+		//2. Commitment. This sub-context covers the commitment phase.
+		//3. Decommitment. This sub-context cover the decommitment phase.
+		//Public context
+		{
+			// Create cyclic group G_q (modulo 167)
+			CyclicGroup cyclicGroup = GStarModSafePrime.getInstance(167);
+
+			// Establish the common reference String by a given ReferenceRandomByteSequence
+			ReferenceRandomByteSequence referenceRandomByteSequence = ReferenceRandomByteSequence.getInstance(ByteArray.getInstance("Hello World".getBytes()));
+
+			Example.printLine("Cylic Group (G_q)", cyclicGroup);
+
+			Element commitmentElement = null;
+			Element randomizationElement = null;
+			Element messageElement = null;
+
+			// Commitment context
+			{
+				// Create commitment scheme to be used
+				PedersenCommitmentScheme commitmentScheme = PedersenCommitmentScheme.getInstance(cyclicGroup, referenceRandomByteSequence);
+
+				// Create message (m) and randomization (r) to commit
+				Element message = commitmentScheme.getMessageSpace().getElementFrom(42);
+				Element randomization = commitmentScheme.getRandomizationSpace().getElementFrom(7); //Not really random!
+
+				// Create commitment (h^m * g^r mod p)
+				Element commitment = commitmentScheme.commit(message, randomization);
+
+				//Publish commitment
+				commitmentElement = commitment;
+
+				Example.printLine("Message Generator (g)", commitmentScheme.getMessageGenerator());
+				Example.printLine("Randomization Generator (h)", commitmentScheme.getRandomizationGenerator());
+				Example.printLine("Commitment", commitment);
+
+				//Publish for decommitment
+				randomizationElement = randomization;
+				messageElement = message;
+
+			}
+
+			//Decommitment context
+			{
+				// (Re-)Create commitment scheme to be used
+				PedersenCommitmentScheme commitmentScheme = PedersenCommitmentScheme.getInstance(cyclicGroup, referenceRandomByteSequence);
+
+				// Get the commitment
+				Element commitment = commitmentElement;
+
+				// Get the message (m)
+				Element message = messageElement;
+
+				// Get the randomization (r)
+				Element randomization = randomizationElement;
+
+				// Decommit
+				BooleanElement result = commitmentScheme.decommit(message, randomization, commitment);
+
+				Example.printLine("Message Generator (g)", commitmentScheme.getMessageGenerator());
+				Example.printLine("Randomization Generator (h)", commitmentScheme.getRandomizationGenerator());
+				Example.printLine("Result", result);
+			}
+		}
+	}
+
+	public static void example4() {
+		// Here the generators are chosen by the use of a common reference string (consensed from outside).
+		// randomization is provided by using the internal true random byte sequence generator
+
+		// randomization is provided by the true random byte sequence.
+		//This example demonstrates the different steps in three Contexts:
+		//1. Public parameters. Within this context the math and the common reference string is defined.
+		//2. Commitment. This sub-context covers the commitment phase.
+		//3. Decommitment. This sub-context cover the decommitment phase.
+		//Public context
+		{
+			// Create cyclic group G_q (modulo 167)
+			CyclicGroup cyclicGroup = GStarModSafePrime.getInstance(167);
+
+			ReferenceRandomByteSequence referenceRandomByteSequence = ReferenceRandomByteSequence.getInstance(ByteArray.getInstance("Stock Exchange".getBytes()));
+
+			Example.printLine("Cylic Group (G_q)", cyclicGroup);
+
+			Element commitmentElement = null;
+			Element randomizationElement = null;
+			Element messageElement = null;
+
+			// Commitment context
+			{
+				// Create commitment scheme to be used
+				PedersenCommitmentScheme commitmentScheme = PedersenCommitmentScheme.getInstance(cyclicGroup, referenceRandomByteSequence);
+
+				// Create message (m) and randomization (r) to commit
+				Element message = commitmentScheme.getMessageSpace().getElementFrom(42);
+				Element randomization = commitmentScheme.getRandomizationSpace().getRandomElement();
+
+				// Create commitment (h^m * g^r mod p)
+				Element commitment = commitmentScheme.commit(message, randomization);
+
+				//Publish commitment
+				commitmentElement = commitment;
+
+				Example.printLine("Message Generator (g)", commitmentScheme.getMessageGenerator());
+				Example.printLine("Randomization Generator (h)", commitmentScheme.getRandomizationGenerator());
+				Example.printLine("Commitment", commitment);
+
+				//Publish for decommitment
+				randomizationElement = randomization;
+				messageElement = message;
+			}
+
+			//Decommitment context
+			{
+				// (Re-)Create commitment scheme to be used
+				PedersenCommitmentScheme commitmentScheme = PedersenCommitmentScheme.getInstance(cyclicGroup, referenceRandomByteSequence);
+
+				// Get the commitment
+				Element commitment = commitmentElement;
+
+				// Get the message (m)
+				Element message = messageElement;
+
+				// Get the randomization (r)
+				Element randomization = randomizationElement;
+
+				// Decommit
+				BooleanElement result = commitmentScheme.decommit(message, randomization, commitment);
+
+				Example.printLine("Message Generator (g)", commitmentScheme.getMessageGenerator());
+				Example.printLine("Randomization Generator (h)", commitmentScheme.getRandomizationGenerator());
+				Example.printLine("Result", result);
+			}
+		}
 	}
 
 	public static void main(final String[] args) {
