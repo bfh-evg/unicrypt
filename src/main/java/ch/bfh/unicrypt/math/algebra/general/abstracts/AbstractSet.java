7dc2819886c1324cba1fde13210b660277422252
Christian Lutz <lutz.chrigu@gmail.com>
2014-09-16 13:51:44 +0200

    Delete System.out.print...

--- src/main/java/ch/bfh/unicrypt/math/algebra/general/abstracts/AbstractSet.java
+++ src/main/java/ch/bfh/unicrypt/math/algebra/general/abstracts/AbstractSet.java
@@ -218,8 +218,6 @@ public abstract class AbstractSet<E extends Element<V>, V extends Object>
 
 	@Override
 	public final E getElement(V value) {
-		System.out.println(this);
-		System.out.println(value);
 		if (!this.contains(value)) {
 			throw new IllegalArgumentException();
 		}
