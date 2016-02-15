9a3a947d8c4da42b6af03417b7325eff74bae33d
Rolf Haenni <rolf.haenni@bfh.ch>
2016-02-15 16:22:47 +0100

    LRUHashMap

---
+++ src/test/java/ch/bfh/unicrypt/helper/map/LRUHashMapTest.java
@@ -0,0 +1,80 @@
+/*
+ * UniCrypt
+ *
+ *  UniCrypt(tm): Cryptographic framework allowing the implementation of cryptographic protocols, e.g. e-voting
+ *  Copyright (C) 2016 Bern University of Applied Sciences (BFH), Research Institute for
+ *  Security in the Information Society (RISIS), E-Voting Group (EVG)
+ *  Quellgasse 21, CH-2501 Biel, Switzerland
+ *
+ *  Licensed under Dual License consisting of:
+ *  1. GNU Affero General Public License (AGPL) v3
+ *  and
+ *  2. Commercial license
+ *
+ *
+ *  1. This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *  2. Licensees holding valid commercial licenses for UniCrypt may use this file in
+ *   accordance with the commercial license agreement provided with the
+ *   Software or, alternatively, in accordance with the terms contained in
+ *   a written agreement between you and Bern University of Applied Sciences (BFH), Research Institute for
+ *   Security in the Information Society (RISIS), E-Voting Group (EVG)
+ *   Quellgasse 21, CH-2501 Biel, Switzerland.
+ *
+ *
+ *   For further information contact <e-mail: unicrypt@bfh.ch>
+ *
+ *
+ * Redistributions of files must retain the above copyright notice.
+ */
+package ch.bfh.unicrypt.helper.map;
+
+import java.util.Map;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+
+/**
+ *
+ * @author rolfhaenni
+ */
+public class LRUHashMapTest {
+
+	@Test
+	public void test() {
+
+		Map<Integer, Integer> map = LRUHashMap.getInstance();
+		for (int i = 1; i <= 150; i++) {
+			map.put(i, i);
+		}
+		assertNull(map.get(0));
+		assertNull(map.get(1));
+		assertNull(map.get(50));
+		assertNotNull(map.get(51));
+		assertNotNull(map.get(52));
+		assertNotNull(map.get(150));
+		assertNull(map.get(151));
+
+		map.put(151, 151);
+		assertNull(map.get(53));
+		assertNotNull(map.get(54));
+		assertEquals(100, map.size());
+		map.remove(54);
+		assertEquals(99, map.size());
+
+	}
+
+}
